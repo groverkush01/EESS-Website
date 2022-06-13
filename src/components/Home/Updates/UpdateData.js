3b11d4c392e0285868a2e47db12b33a5adf3b85c
SANSKRITI SHARAN <sanskriti_s@ee.iitr.ac.in>
2022-06-13 15:38:21 +0530

    Updates

---
+++ src/components/Home/Updates/UpdateData.js
@@ -0,0 +1,37 @@
+import Img1 from "../../../Assets/img_home/update_imgs/Recruitments.jpg";
+import Img2 from "../../../Assets/img_home/update_imgs/Farewell.jpg";
+import Img3 from "../../../Assets/img_home/update_imgs/Internship.jpg";
+import Img4 from "../../../Assets/img_home/update_imgs/FreshersVideo.png";
+
+const UpdateData = [
+    {
+        index:1, //Key
+        image:Img1,
+        header: "Recruitments",
+        description:"How's the josh? Well,pretty high as we received tremendous amount of responses. Stay tuned to know who makes the cut!",
+        link: "https://youtu.be/1bmihxBedxo"
+    },
+    {
+        index:2,
+        image:Img2,
+        header: "Farewell 2022",
+        description:"Get a glimpse down the memory lane and relive the moments as we celebrate the amazing journey of our most recent graduates!! ",
+        link: "https://youtu.be/1bmihxBedxo"
+    },
+    {
+        index:3, 
+        image:Img3,
+        header: "Internship Talks",
+        description:" 'There is guidance for each of us, and by lowly listening we shall hear the right word...'  ~ Ralph Waldo Emerson  Get yourself the answers you have always been searching for and clear your doubts about internships and much more by knowing the journey of our beloved seniors.",
+        link: "https://www.youtube.com/watch?v=1OoglpGoDrA&ab_channel=ElectricalEngineeringStudents%27Society%2CIITR"
+    },
+    {
+        index:4, 
+        image:Img4,
+        header: "Freshers' Video",
+        description:"Every batch has its own quirkiness and uniqueness. Here's a sneak peek into the goofy batch of our freshers this year!",
+        link: "https://www.youtube.com/watch?v=pWzGryQYxeA&ab_channel=ElectricalEngineeringStudents%27Society%2CIITR"
+    },
+];
+
+export default UpdateData;
---
+++ src/components/Home/Updates/Updates.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Update.css";
+import UpdateData from './UpdateData';
+import GlassContainer from '../../UI/GlassContainer/GlassContainer'
+import Heading from '../../UI/Heading/Heading';
+import ButtonAnimate from '../../UI/Button_animated/ButtonAnimate';
+
+export default function Updates() {
+  return (
+    <div className="UpdateContainer">
+        <GlassContainer>
+            <Heading name="Updates"/>
+            <div className="Update_card_container">
+                {UpdateData.map((obj,indx)=>{
+                    return(
+                        <div key={indx} className='Update_card'>
+                            <img src={obj.image} alt=""/>
+                            <div className="Update_card_content">
+                                <div className="Update_heading">{obj.header}</div>
+                                <div className="Update_descp">{obj.description}</div>
+                                <ButtonAnimate link={obj.link} text="Know More"/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </GlassContainer>
+    </div>
+  )
+}
